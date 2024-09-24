628e3efc51ebf02b856b8c31132a365efe841340
willsf2021 <willsf2021@gmail.com>
2024-09-23 21:01:05 -0300

    Update Ex3.cpp
    Added clean code

--- exercicios-ifElse-opLogicos/Ex3.cpp
+++ exercicios-ifElse-opLogicos/Ex3.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 /*
-3) Uma empresa de vendas tem trÍs corretores. A empresa paga ao corretor uma comiss„o calculada de acordo com o valor de suas vendas. 
-Se o valor da venda de um corretor for maior que R$ 50.000.00 a comiss„o ser· de 12% do valor vendido. 
-Se o valor da venda do corretor estiver entre R$ 30.000.00 e R$ 50.000.00 (incluindo extremos) a comiss„o ser· de 9.5%. Em qualquer outro caso, a comiss„o ser· de 7%. 
-Escreva um algoritmo que mostre o nome, valor da venda e comiss„o de cada corretor. 
-A saÌda deve mostrar tambÈm o total de vendas da empresa.
+3) Uma empresa de vendas tem tr√™s corretores. A empresa paga ao corretor uma comiss√£o calculada de acordo com o valor de suas vendas. 
+Se o valor da venda de um corretor for maior que R$ 50.000.00 a comiss√£o ser√° de 12% do valor vendido. 
+Se o valor da venda do corretor estiver entre R$ 30.000.00 e R$ 50.000.00 (incluindo extremos) a comiss√£o ser√° de 9.5%. Em qualquer outro caso, a comiss√£o ser√° de 7%. 
+Escreva um algoritmo que mostre o nome, valor da venda e comiss√£o de cada corretor. 
+A sa√≠da deve mostrar tamb√©m o total de vendas da empresa.
 */
 
 int main() {
@@ -17,15 +17,15 @@ int main() {
 	float vend_claudio = 0, vend_carla = 0, vend_roberto = 0, comissao_claudio = 0, comissao_carla = 0, comissao_roberto = 0;
 	
 	cout << "+======================+ \n";
-	cout << "| C·lculo de Comissıes | \n";
+	cout << "| C√°lculo de Comiss√µes | \n";
 	cout << "+======================+ \n";
 	
-	cout << "Valor de Vendas Corretor Cl·udio: R$" << endl;
+	cout << "Valor de Vendas Corretor Cl√°udio: R$" << endl;
 	cin >> vend_claudio;
 	
 	if(vend_claudio > 50000) {
 		comissao_claudio = 0.12;
-	}else if (vend_claudio >= 30000 && vend_claudio <= 50000){
+	}else if (vend_claudio >= 30000){
 		comissao_claudio = .095;
 	}else{
 		comissao_claudio = 0.7;
@@ -37,7 +37,7 @@ int main() {
 	
 	if(vend_carla > 50000) {
 		comissao_carla = 0.12;
-	}else if (vend_carla >= 30000 && vend_carla <= 50000){
+	}else if (vend_carla >= 30000){
 		comissao_carla = .095;
 	}else{
 		comissao_carla = 0.7;
@@ -48,16 +48,16 @@ int main() {
 	
 	if(vend_roberto > 50000) {
 		comissao_roberto = 0.12;
-	}else if (vend_roberto >= 30000 && vend_roberto <= 50000){
+	}else if (vend_roberto >= 30000){
 		comissao_roberto = .095;
 	}else{
 		comissao_roberto = 0.7;
 	}
 	
 	
-	cout << "Comiss„o do Corretor Cl·udio R$" << vend_claudio * comissao_claudio << endl;
-	cout << "Comiss„o do Corretor Carla R$" << vend_carla * comissao_carla << endl;
-	cout << "Comiss„o do Corretor Roberto R$" << vend_roberto * comissao_roberto << endl;
+	cout << "Comiss√£o do Corretor Cl√°udio R$" << vend_claudio * comissao_claudio << endl;
+	cout << "Comiss√£o do Corretor Carla R$" << vend_carla * comissao_carla << endl;
+	cout << "Comiss√£o do Corretor Roberto R$" << vend_roberto * comissao_roberto << endl;
 	
 	
 	return 0;
